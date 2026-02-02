4c036a4478ec07c58b740c9a31073b156524019f
Kenan-LCF <kenan.Lacrampe-cuyaubere-flieutete@epitech.eu>
2026-02-02 15:31:33 +0100

    Create Experience

---
+++ Portfolio/src/components/Experience.tsx
@@ -0,0 +1,14 @@
+const Experience = () => {
+  return (
+    <div>
+            <h1 className="text-5xl text-white text-center font-bold">Parcours</h1>
+        
+        <div>
+          {/* Centrer la div */}
+            <div className="bg-white h-[500px] w-[5px] mt-20 mx-auto"></div>
+        </div>
+    </div>
+  )
+};
+
+export default Experience;
