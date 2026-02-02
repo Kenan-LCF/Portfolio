882136d17d30f26f0f70aeddf4c4283e6aa2cbd0
Kenan-LCF <kenan.Lacrampe-cuyaubere-flieutete@epitech.eu>
2026-02-02 15:31:54 +0100

    Update app

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,10 +1,14 @@
 import Navigation from './components/Navigation.tsx'  
 import Hero from './components/Hero.tsx'
+import Profile from './components/Profile.tsx'
+import Experience from './components/Experience.tsx'
 function App() {
   return (
     <div>
       <Navigation />
       <Hero />
+      <Profile />
+      <Experience />
     </div>
   )
 }
